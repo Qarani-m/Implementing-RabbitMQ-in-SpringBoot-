a3ce5e986f3be2bd600e3f7bd9beb3c2884755db
qarani-m <emqarani@gmail.com>
2024-02-17 10:49:29 +0300

    Added concurency

--- src/main/java/mqconsumer/MessageListener.java
+++ src/main/java/mqconsumer/MessageListener.java
@@ -6,12 +6,15 @@ import mqconsumer.entity.CustomMessage;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
 @Component
 public class MessageListener {
 
-    @RabbitListener(queues = MQConfig.messageQueue)
+    @RabbitListener(queues = MQConfig.messageQueue, concurrency = "5")
     public void listenMessageQueue(CustomMessage message) {
         System.out.println("Received message: " + message);
+        // Process the message here
     }
-
 }
